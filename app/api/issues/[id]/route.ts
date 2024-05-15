dc6b2b35d48395a004b22974b5ac3950d813bf4f
Alireza Askari <alireza2023asgari@gmail.com>
2024-05-15 16:59:54 +0330

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,10 @@
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+interface Params {
+  params: { id: string };
+}
+export async function PATCH(request: NextRequest, { params }: Params) {
   const body = await request.json();
 
   const validton = issueSchema.safeParse(body);
@@ -29,4 +28,14 @@ export async function PATCH(
     },
   });
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(request: NextRequest, { params }: Params) {
+  const issue = prisma.isssue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+  await prisma.isssue.delete({ where: { id: parseInt(params.id) } });
+  return NextResponse.json({});
 }
