2613d708c5f939a120c6466e1df9aad731f52b92
Alireza Askari <alireza2023asgari@gmail.com>
2024-05-23 12:01:17 +0330

    Build the barchart compoent

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
 import IssueSummery from "./IssueSummery";
 import LatestIssue from "./LatestIssue";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
   const open = await prisma.isssue.count({ where: { state: "OPEN" } });
@@ -8,5 +9,5 @@ export default async function Home() {
     where: { state: "IN_PROGRESS" },
   });
   const closed = await prisma.isssue.count({ where: { state: "CLOSED" } });
-  return <IssueSummery open={open} inProgress={inProgress} closed={closed} />;
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
