5b072a7c436bb39f6c5e6d47024efd5015110ee5
Alireza Askari <alireza2023asgari@gmail.com>
2024-05-25 13:54:45 +0330

    Add loading to app folder and remove suspens

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -2,14 +2,11 @@
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import IssueStatusFilter from "./IssueStatusFilter";
-import { Suspense } from "react";
 
 const IssueActions = () => {
   return (
     <Flex justify={"between"}>
-      <Suspense>
         <IssueStatusFilter />
-      </Suspense>
       <Button>
         <Link href="/issues/new">New issue</Link>
       </Button>
